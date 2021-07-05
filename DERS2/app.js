ab13b21f7dbe2fe15dfeedb74fe4bba0b5afd276
İhsan ÇETİN <ihsancetin92@gmail.com>
2021-07-05 11:47:53 +0300

    Add files via upload

---
+++ DERS2/app.js
@@ -0,0 +1,2 @@
+console.log("I'm from outsite")
+console.log("merhaba")
---
+++ DERS3/app.js
@@ -0,0 +1,107 @@
+
+// let a;
+// let aa;
+// let aaa;
+
+
+// let abc = function add (a,b){
+//     console.log(a+b)
+// }
+
+// abc(3,4)
+
+
+// function stack (){
+//     stack()
+
+// }
+
+// stack()
+
+
+// let a = 5;
+// console.log(a)
+
+// {
+//     let a = 8;
+//     console.log(a)
+    
+// }
+
+// console.log(a)
+
+// var a = 6;
+// var b = 2;
+
+/* console.log(a+b) //8
+console.log(a-b) //4
+console.log(a*b) //12
+console.log(a/b) //3
+console.log(a**b) //36 */
+
+
+//ıncrement decrement
+// var a = 6;
+// var b = 3;
+
+// let a = --b // Bu satırda  b=2
+// console.log({b})  //bu satırda b=2
+// console.log({a})
+
+// let a = b-- // Bu satırda  b=3
+// console.log({b}) //bu satırda b=2
+// console.log({a})
+
+// var b = 5;
+
+// console.log(++b) //6
+// console.log(b++) // 5
+// console.log({b}) // 6
+
+// var a = ++b //b = b + 1, a =b
+// console.log({b}) //b =6
+// console.log({a})//6
+
+
+// a = b++ // a=b , b = b+1
+// console.log({b}) //6
+// console.log({a})//5
+
+// b = 6;
+// a =2;
+
+// console.log(b%a)
+
+
+//Assignment Operatos
+
+// var a = 6;
+// var b = 2;
+
+// console.log(a=b, {a},{b})
+// console.log(a+=b, {a},{b}) 
+// console.log(a-=b, {a},{b})
+// console.log(a/=b, {a},{b})
+// console.log(a*=b, {a},{b})
+// console.log(a%=b, {a},{b})
+
+// var a = 2;
+// var b = '2';
+
+// console.log('strict equality',a === b) //false
+// console.log('equality',a == b) //true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
